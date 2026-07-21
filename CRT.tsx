663a0667ff10cf5ec57dfe913247541ca47730db
Lalit Kumar <lalitkumar2481211@gmail.com>
2026-07-22 03:10:50 +0530

    Update CRT.tsx

--- CRT.tsx
+++ CRT.tsx
@@ -1,40 +1,26 @@
 import "./CRT.css";
-
 interface CRTProps{
-
     onExit: () => void;
 
 }
 
 function CRT({ onExit }: CRTProps){
-
     return(
-
         <div className="crt-workspace">
-
             <button
                 className="exit-button"
                 onClick={onExit}
             >
 
                 [ EXIT ]
-
             </button>
-
             <div className="crt-menu">
-
                 <button>Photography</button>
-
                 <button>Projects</button>
-
                 <button>ASCII</button>
-
                 <button>Experiments</button>
-
                 <button>Archives</button>
-
             </div>
-
         </div>
 
     );
